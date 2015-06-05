0f6594f55ccaeb74b56366c86b66d1f85def8ba8
이정우 <kenlima@wemakeprice.com>
2015-06-05 19:14:46 +0900

    fully executable

--- simpletool.sh
+++ simpletool.sh
@@ -1,79 +1,42 @@
 #!/bin/sh
-
-SIMPLETOOL_HOME=/usr/local/simpletool
-PIDPATH=$SIMPLETOOL_HOME/pid
-PIDFILE=$PIDPATH/simpletool.pid
-STARTSHELL=`java -Dspring.profiles.active=real -jar $SIMPLETOOL_HOME/simpletool-0.1.0.jar > $SIMPLETOOL_HOME/simpletool.log &`
-PID=0
-
-if [ ! -d "$PIDPATH" ]; then
-  mkdir $PIDPATH
-fi
-
-if [ -e $PIDFILE ]; then
-    PID=`cat $PIDFILE`
-    if [ "q$PID" != "q" ] && kill -0 $PID 2> /dev/null ; then
-	STATUS="SimpleTool (pid $PID) running"
-    else
-	STATUS="SimpleTool (pid $PID?) not running"
-    fi 
-    if [ "x" == "x$PID" ]; then
-        PID=0
-    fi
-else
-  STATUS="SimpleTool (no pid file) not running"
-fi
-
-case "$1" in
+SERVICE_NAME=SimpleTool
+PATH_TO_JAR=/usr/local/simpletool/simpletool-0.1.0.jar
+PID_PATH_NAME=/usr/local/simpletool/simpletool.pid
+case $1 in
     start)
-        if [ 0 -ne $PID ]; then
-            running=`ps --pid $PID | grep $PID |wc -l`
-            if [ $running ]; then
-                echo "Server is already running"
-                exit 1
-            fi
-
-            echo "Server does not appear to be running, but old PID file exists. Removing..."
-            rm $PIDFILE
-            PID=0
+        echo "Starting $SERVICE_NAME ..."
+        if [ ! -f $PID_PATH_NAME ]; then
+            nohup java -Dspring.profiles.active=real -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+                        echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is already running ..."
         fi
-
-        echo "Starting SimpleTool Server ... ... ..."
-        echo $STARTSHELL
-        sleep 5 
-        PID=`cat $PIDFILE`
-        echo "SimpleTool Service pid $PID running...."
-        echo "done"
-        ;;
+    ;;
     stop)
-        if [ 0 -eq $PID ]; then
-            echo "Server was not running"
-            exit 1
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stoping ..."
+            kill $PID;
+            echo "$SERVICE_NAME stopped ..."
+            rm $PID_PATH_NAME
+        else
+            echo "$SERVICE_NAME is not running ..."
         fi
-
-        echo "Attempting to kill process $PID (SimpleTool)"
-        kill $PID
-        while [ 1 ];
-        do
-                if ps --pid $PID > /dev/null; then
-                        echo -n ".";
-                        sleep 1;
-                else
-                        break;
-                fi
-        done
-        echo "done"
-        ;;
+    ;;
     restart)
-        $0 stop
-        $0 start
-        ;;
-    status)
-        echo $STATUS;
-	;;
-    *)
-        echo "Usage: "`basename $0`" (start|stop|restart|status)"
-        exit 1
-        ;;
-esac
-
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stopping ...";
+            kill $PID;
+            echo "$SERVICE_NAME stopped ...";
+            rm $PID_PATH_NAME
+            echo "$SERVICE_NAME starting ..."
+            nohup java -Dspring.profiles-active=real -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+                        echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+esac 
